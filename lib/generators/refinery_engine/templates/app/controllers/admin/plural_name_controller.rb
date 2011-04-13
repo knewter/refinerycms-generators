b35eb0b2a543c6b03e2f73fe3251f571bdc9f844
Philip Arndt <parndt@gmail.com>
2011-04-13 14:35:29 +1200

    Use _path routes instead of _url routes so they're relative to the site.

--- lib/generators/refinery_engine/templates/app/controllers/admin/plural_name_controller.rb
+++ lib/generators/refinery_engine/templates/app/controllers/admin/plural_name_controller.rb
@@ -3,7 +3,7 @@ module Admin
 
     crudify :<%= singular_name %><% if (title = attributes.detect { |a| a.type.to_s == "string" }).present? and title.name != 'title' %>,
             :title_attribute => '<%= title.name %>'<% end %><% if plural_name == singular_name %>,
-              :redirect_to_url => :admin_<%= singular_name %>_index_url
+              :redirect_to_url => :admin_<%= singular_name %>_index_path
 <% end %>, :xhr_paging => true
 
   end
