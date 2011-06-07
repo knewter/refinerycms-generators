2bb7d2cd25a2d2471f24f36cda128eaf1658b938
Josh Adams <josh@isotope11.com>
2011-06-07 05:31:15 -0500

    [controllers] 2 minor tweaks to the generated controllers that didn't make it in before

--- lib/generators/refinery_engine/templates/app/controllers/refinery/admin/plural_name_controller.rb
+++ lib/generators/refinery_engine/templates/app/controllers/refinery/admin/plural_name_controller.rb
@@ -1,8 +1,8 @@
-module Refinery
+module ::Refinery
   module Admin
-    class <%= class_name.pluralize %>Controller < Admin::BaseController
+    class <%= class_name.pluralize %>Controller < ::Admin::BaseController
 
-      crudify :<%= singular_name %><% if (title = attributes.detect { |a| a.type.to_s == "string" }).present? and title.name != 'title' %>,
+      crudify :'refinery/<%= singular_name %>'<% if (title = attributes.detect { |a| a.type.to_s == "string" }).present? and title.name != 'title' %>,
               :title_attribute => '<%= title.name %>'<% end %><% if plural_name == singular_name %>,
                 :redirect_to_url => :admin_<%= singular_name %>_index_path
   <% end %>, :xhr_paging => true
